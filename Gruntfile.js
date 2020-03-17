bf5ec581cb282d1b6a33416ddcb3f29e72b9bb4e
jilinlin <1695729490@qq.com>
2020-03-17 22:32:04 +0800

    add

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,21 +2,26 @@
 module.exports=function(grunt){
     //less插件构建的配置信息
     grunt.initConfig({
-        mocha:{
-            test:{
-                src:'test/index.html'
-            },
+        run:{
+            api:{
+                options:{wait:false},
+                args:['./app.js']
+            }
+        },
+        mochacli:{
+            all:['test/*.js'],
             options:{
-                run:true,
-                reportor:"Dot"
+                reportor:"spec",
+                bail:true
             }
         }
     });
 
     //加载插件
-    grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-mocha-cli');
+    grunt.loadNpmTasks('grunt-run');
 
     //定义构建的任务清单
-    grunt.registerTask('default',['mocha']);
+    grunt.registerTask('default',['run','mochacli','stop:api']);
     
 }
