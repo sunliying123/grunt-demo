31971e1c7977a29f03eaaa0a3f0bd87eab2134ee
jilinlin <1695729490@qq.com>
2020-03-17 18:38:52 +0800

    ''

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,21 @@
 module.exports=function(grunt){
     //less插件构建的配置信息
     grunt.initConfig({
-        eslint:{
-            options:{
-                csslintrc:'./.eslintrc.json'
+        mocha:{
+            test:{
+                src:'test/index.html'
             },
-            target:['*.js']
+            options:{
+                run:true,
+                reportor:"Dot"
+            }
         }
     });
 
     //加载插件
-    grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-mocha');
 
     //定义构建的任务清单
-    grunt.registerTask('default',['eslint']);
+    grunt.registerTask('default',['mocha']);
     
 }
